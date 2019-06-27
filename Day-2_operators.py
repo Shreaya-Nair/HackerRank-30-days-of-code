030e8eac7f1b71f49edee85b7fd8f03ef700a764
Shreaya-Nair <52288571+Shreaya-Nair@users.noreply.github.com>
2019-06-27 20:40:24 +0530

    Day-2: Operators

---
+++ Day-2_operators.py
@@ -0,0 +1,8 @@
+# Complete the solve function b
+meal_cost = float(input())
+tip_percent = int(input())
+tax_percent = int(input())
+tip = meal_cost*tip_percent/100    
+tax = meal_cost*tax_percent/100
+total_cost=round(meal_cost+tip+tax)
+print(total_cost)
